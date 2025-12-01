dfacd338eda749ae7db93ad290e519ce6912d3a5
RA7 <169072120+Ruslan-A7@users.noreply.github.com>
2025-12-01 18:28:59 +0200

    update

--- src/TypesEventsEnum.php
+++ src/TypesEventsEnum.php
@@ -2,6 +2,16 @@
 
 namespace RA7\Framework\System\Enums;
 
+/**
+ * Список доступних типів подій.
+ *
+ * Дозволяє розділити події в залежності від їх призначення.
+ *
+ * @author Ruslan_A7 (RA7) <https://ra7.iuid.cc>
+ * Код може містити деякі частини, що були створені за допомогою ChatGPT.
+ * @license RA7 Open Free License <https://ra7.iuid.cc/LICENSE>
+ * @github <https://github.com/Ruslan-A7>
+ */
 enum TypesEventsEnum {
 
     /** Інформування */
